45cc49a3827e06455700af249cc7d10d3c3deac7
Vlad <69111575+Vlad-Cr@users.noreply.github.com>
2021-05-16 16:23:18 +0300

    Add Get and Delete tests

--- DAL.Tests/BaseRepositoryUnitTests.cs
+++ DAL.Tests/BaseRepositoryUnitTests.cs
@@ -28,5 +28,51 @@ namespace DAL.Tests
 			mockDbSet.Verify(dbSet => dbSet.Add(expectedReport), Times.Once());
 	
 		}
+
+		[Fact]
+		public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
+		{
+			// Arrange
+			DbContextOptions opt = new DbContextOptionsBuilder<ConfectioneryCompanyContext>()
+				.Options;
+			var mockContext = new Mock<ConfectioneryCompanyContext>(opt);
+			var mockDbSet = new Mock<DbSet<Report>>();
+			mockContext.Setup(context => context.Set<Report>()).Returns(mockDbSet.Object);
+
+			Report expectedReport = new Report() { Id = 1 };
+			mockDbSet.Setup(mock => mock.Find(expectedReport.Id)).Returns(expectedReport);
+			var repository = new TestReportRepository(mockContext.Object);
+
+			//Act
+			var actualStreet = repository.Get(expectedReport.Id);
+
+			// Assert
+			mockDbSet.Verify(dbSet => dbSet.Find(expectedReport.Id), Times.Once());
+			Assert.Equal(expectedReport, actualStreet);
+		}
+
+		[Fact]
+		public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
+		{
+			// Arrange
+			DbContextOptions opt = new DbContextOptionsBuilder<ConfectioneryCompanyContext>()
+				.Options;
+			var mockContext = new Mock<ConfectioneryCompanyContext>(opt);
+			var mockDbSet = new Mock<DbSet<Report>>();
+
+			mockContext.Setup(context => context.Set<Report>()).Returns(mockDbSet.Object);
+			var repository = new TestReportRepository(mockContext.Object);
+
+			Report expectedReport = new Report() { Id = 1 };
+			mockDbSet.Setup(mock => mock.Find(expectedReport.Id)).Returns(expectedReport);
+
+			//Act
+			repository.Delete(expectedReport.Id);
+
+			// Assert
+			mockDbSet.Verify(dbSet => dbSet.Find(expectedReport.Id), Times.Once());
+			mockDbSet.Verify(dbSet => dbSet.Remove(expectedReport), Times.Once());
+		}
+
 	}
 }
