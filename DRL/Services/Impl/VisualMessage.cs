d00427620bfb2b7e87c7b25fa9357fb0525df1c2
Vlad <69111575+Vlad-Cr@users.noreply.github.com>
2021-06-01 22:38:23 +0300

    Added adapter pattern

---
+++ DRL/Services/Impl/VisualMessage.cs
@@ -0,0 +1,38 @@
+﻿using DRL.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DRL.Services.Impl
+{
+	public class VisualMessage : IVisualMessage
+	{
+		public DialogResult ShowConfirmMessage(string msg)
+		{
+			return MessageBox.Show(msg, "Confirmation!", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+		}
+
+		public void ShowErrorMessage(string msg)
+		{
+			MessageBox.Show(msg, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		}
+
+		public void ShowInformMessage(string msg)
+		{
+			MessageBox.Show(msg, "Information!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+		}
+
+		public DialogResult ShowQuestionMessage(string msg)
+		{
+			return MessageBox.Show(msg, "Question?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+		}
+
+		public void ShowWarningMessage(string msg)
+		{
+			MessageBox.Show(msg, "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+		}
+	}
+}
---
+++ DRL/Services/Interface/IVisualMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DRL.Services.Interface
+{
+	public interface IVisualMessage
+	{
+		void ShowErrorMessage(string msg);
+		void ShowInformMessage(string msg);
+		void ShowWarningMessage(string msg);
+		DialogResult ShowConfirmMessage(string msg);
+		DialogResult ShowQuestionMessage(string msg);
+	}
+}
